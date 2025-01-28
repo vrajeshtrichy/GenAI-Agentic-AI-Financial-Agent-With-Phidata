ef16f0b10e85d4e125ba16cf7e1844d19d16449d
vrajeshtrichy <47318383+vrajeshtrichy@users.noreply.github.com>
2025-01-28 11:06:25 +0530

    Create DuckDuckGo web search agent using Grop Fast API and Llama 3.3 70b model

--- app.py
+++ app.py
@@ -2,3 +2,14 @@ from phi.agent import Agent
 from phi.model.groq import Groq
 from phi.tools.yfinance import YFinanceTools
 from phi.tools.duckduckgo import DuckDuckGo
+
+# Create DuckDuckGo web search agent
+web_search_agent = Agent(
+    name = "Web Search Agent",
+    role = "Search the web for the information",
+    model = Groq(id = "llama-3.3-70b-versatile"),
+    tools = [DuckDuckGo()],
+    instructions = ["Always include sources"],
+    show_tools_calls = True,
+    markdown = True,
+)
